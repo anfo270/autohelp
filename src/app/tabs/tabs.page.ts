54ab8942ae2e3492b77fd616b20925530e8388d0
anfo270 <andres44chuy@gmail.com>
2024-04-22 00:42:17 -0600

    first commit

---
+++ src/app/tabs/tabs.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { LocalStorageService } from '../services/localstorage.service';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss']
+})
+export class TabsPage {
+
+  constructor(
+  ) {
+  }
+
+}
